37703a436b1671f18bfb96573a4fb127d617cf2d
Nick Mulder <nmulder@outlook.com>
2023-01-02 14:00:53 -0600

    progress into ch. 10

---
+++ src/app/component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Model } from './repository.model';
+
+@Component({
+    selector: "app",
+    templateUrl: "template.html"
+})
+export class ProductComponent {
+    model: Model = new Model();
+
+    getClasses(key: number): string {
+        let product = this.model.getProduct(key);        
+        return "p-2 " + ((product?.price ?? 0) > 50 ? "bg-info" : "bg-warning");
+    }   
+}
