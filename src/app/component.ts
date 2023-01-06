7370e5732e429559ec448d53e4b89d0992eb3218
Nick Mulder <nmulder@outlook.com>
2023-01-05 18:22:01 -0600

    PreChapter 11

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component } from '@angular/core';
 import { Model } from './repository.model';
+import { Product } from './product.model';
 
 @Component({
     selector: "app",
@@ -8,16 +9,42 @@ import { Model } from './repository.model';
 export class ProductComponent {
     model: Model = new Model();
 
-    getClasses(key: number): string {
-        let product = this.model.getProduct(key);        
-        return "p-2 " + ((product?.price ?? 0) > 50 ? "bg-info" : "bg-warning");
+    constructor(ref: ApplicationRef) {
+        (<any>window).appRef = ref;
+        (<any>window).model = this.model;
     }
 
-    getClassMap(key: number) {
-        let product = this.model.getProduct(key);
-        return {
-            "text-center bg-danger": product?.name == "Kayak",
-            "bg-info": (product?.price ?? 0) < 50
-        };
+    getProductByPosition(position: number): Product {
+        return this.model.getProducts()[position];
     }
+
+    getClassesByPosition(position: number): string {
+        let product = this.getProductByPosition(position);
+        return "p-2 " + ((product?.price ?? 0) < 50 ? "bg-info" : "bg-warning");
+    }
+
+    // getClasses(key: number): string {
+    //     let product = this.model.getProduct(key);        
+    //     return "p-2 " + ((product?.price ?? 0) > 50 ? "bg-info" : "bg-warning");
+    // }
+
+    // getClassMap(key: number) {
+    //     let product = this.model.getProduct(key);
+    //     return {
+    //         "text-center bg-danger": product?.name == "Kayak",
+    //         "bg-info": (product?.price ?? 0) < 50
+    //     };
+    // }
+
+    // fontSizeWithUnits: string = "30px";
+    // fontSizeWithoutUnits: string = "30";
+
+    // getStyles(key: number) {
+    //     let product = this.model.getProduct(key);
+    //     return {
+    //         fontSize: "30px",
+    //         "margin.px": 100,
+    //         color: (product?.price ?? 0) > 50 ? "red" : "green"
+    //     };
+    // }
 }
