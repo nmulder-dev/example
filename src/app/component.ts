144811be5b18719c8cbcb48947b017f4c07f5c4d
Nick Mulder <nmulder@outlook.com>
2023-01-12 18:47:40 -0600

    chapter 12, form validation

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,6 +1,7 @@
-import { ApplicationRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { Model } from './repository.model';
 import { Product } from './product.model';
+import { NgModel, ValidationErrors, NgForm } from '@angular/forms';
 
 @Component({
     selector: "app",
@@ -18,15 +19,61 @@ export class ProductComponent {
         return this.model.getProducts();
     }
     
-    selectedProduct: string | undefined;
+    // selectedProduct: string | undefined;
 
-    getSelected(product: Product): boolean {
-        return product.name == this.selectedProduct;
+    // getSelected(product: Product): boolean {
+    //     return product.name == this.selectedProduct;
+    // }    
+
+    // handleInputEvent(ev: Event) {
+    //     if (ev.target instanceof HTMLInputElement) {
+    //         this.selectedProduct = ev.target.value;
+    //     }
+    // }
+
+    newProduct: Product = new Product();
+
+    get jsonProduct() {
+        return JSON.stringify(this.newProduct);
     }
 
-    handleInputEvent(ev: Event) {
-        if (ev.target instanceof HTMLInputElement) {
-            this.selectedProduct = ev.target.value;
+    addProduct(p: Product) {
+        console.log("New Product: " + this.jsonProduct);
+    }
+
+    getMessages(errs: ValidationErrors | null, name: string): string[] {
+        let messages: string[] = [];
+        for (let errorName in errs) {
+            switch (errorName) {
+                case "required":
+                    messages.push(`You must enter a ${name}`);
+                    break;
+                case "minlength":
+                    messages.push(`A ${name} must be at least 
+                    ${errs['minlength'].requiredLength}`);
+                    break;
+                case "pattern":
+                    messages.push(`The ${name} contains illegal characters`);
+                    break;
+            }
+        }
+        return messages;
+    }
+
+    getValidationMessages(state: NgModel, thingName?: string) {
+        let thing: string = state.path?.[0] ?? thingName;
+        return this.getMessages(state.errors, thing);
+    }
+
+    formSubmitted: boolean = false;
+
+    submitForm(form: NgForm) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            form.resetForm();
+            this.formSubmitted = false;
         }
     }
 }
